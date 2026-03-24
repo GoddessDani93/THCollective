08685a365a7ac28df961fa324bba73b10c20b2a1
GoddessDani93 <114428099+GoddessDani93@users.noreply.github.com>
2026-03-24 10:01:52 -0500

    Create About Us page component

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to THCollective! We are a community focused on sharing knowledge and resources to empower individuals.</p>
+            <p>Our mission is to foster collaboration and support among members, promoting growth and excellence.</p>
+        </div>
+    );
+};
+
+export default About;
