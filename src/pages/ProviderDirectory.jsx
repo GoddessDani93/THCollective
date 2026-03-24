c2e94d456d140657317ea9265e84cbea47dc67b1
GoddessDani93 <114428099+GoddessDani93@users.noreply.github.com>
2026-03-24 10:01:33 -0500

    Create Provider Directory page component

---
+++ src/pages/ProviderDirectory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProviderDirectory = () => {
+    return (
+        <div>
+            <h1>Provider Directory</h1>
+            {/* Add provider listings here */}
+        </div>
+    );
+};
+
+export default ProviderDirectory;
